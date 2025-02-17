33272abc2d059aa0e8f0ef7ff160a6e41170c5f6
ScappaMarco <mscappa0@gmail.com>
2025-02-17 23:56:05 +0100

    es2 continue

--- NumberPredictionML/numberPrediction.py
+++ NumberPredictionML/numberPrediction.py
@@ -1,5 +1,11 @@
-from torchvision import datasets, transforms 
+#dependencies
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch import nn, save, load
+from torch.optim import Adam
+from torch.utils.data import DataLoader
 
-train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=transforms.ToTensor())
+#defianiamo i due dataset principali: uno per il training e uno per il test
+train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=ToTensor()) #train set
 
-test_set = datasets.MNIST(root='./data', train=False, download=True, transform=transforms.ToTensor())
+test_set = datasets.MNIST(root='./data', train=False, download=True, transform=ToTensor()) #test set
