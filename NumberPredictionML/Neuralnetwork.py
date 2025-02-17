355497b0ea5244549149177faa85388c41228df8
ScappaMarco <mscappa0@gmail.com>
2025-02-18 00:52:38 +0100

    training loop added

--- NumberPredictionML/Neuralnetwork.py
+++ NumberPredictionML/Neuralnetwork.py
@@ -1,8 +1,18 @@
 from torch import nn
 
+#Definizione modello di classificazione di tipo multiclass
 class ImageClassifier(nn.Module):
+    #definzione rete neurale tramile MLP (strati lineari)
     def __init__(self):
         super().__init__()
+        self.model = nn.Sequential(
+            nn.Flatten(), #tramiote la funzione Flatten() appiattisco l'immagine 28x28 in un vettore 
+            nn.Linear(28*28, 128), #primo strato connesso (28x28 neuroni di input, 128 neuroni di output)
+            nn.ReLU(), #funziine di attivazione Rectified Liner Unit 
+            nn.Linear(128, 64), #secondo strato (128 neuroni di input (output primo strato) e 64 neuroni di output)
+            nn.ReLU(), #funziine di attivazione Rectified Liner Unit 
+            nn.Linear(64, 10) #terzo e ultimo strato, in cui abbiamo 64 neuroni di input (output strato precedente) e 10 neuroni di output, che corrispondono alle 10 classi disponibili (0-9)
+        )
     
     def forward(self, x):
-        pass
+        return self.model(x)
--- NumberPredictionML/numberPrediction.py
+++ NumberPredictionML/numberPrediction.py
@@ -4,8 +4,37 @@ from torchvision.transforms import ToTensor
 from torch import nn, save, load
 from torch.optim import Adam
 from torch.utils.data import DataLoader
+import Neuralnetwork #classe della neural network definita da me
 
-#defianiamo i due dataset principali: uno per il training e uno per il test
+#definzizione dataset e dataloader per il set di dati per il training
 train_dataset = datasets.MNIST(root='./data', train=True, download=True, transform=ToTensor()) #train set
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True) #fissiamo la dimensione dei batch a 32 per volta
 
+#definzione dataset e dataloader per il set di dati per il test
 test_set = datasets.MNIST(root='./data', train=False, download=True, transform=ToTensor()) #test set
+test_loader = DataLoader(test_set, batch_size=32, shuffle=True) #fissiamo la dimensione dei batrch a 32
+
+mlp = Neuralnetwork() #istanza della rerte neurale creata
+
+#definizione delle loss function e della funzione di ottimizzazione
+loss_func = nn.CrossEntropyLoss() #loss
+optimizer = Adam(mlp.parameters(), lr=0.0001) #optimization
+
+#adesso bisogna creare il ciclo per addestrare la rete neurale tramite n epoche
+num_epochs = 10 #numero di training steps
+for epoch in range(num_epochs):
+    mlp.train()
+    for images, labels in train_loader:
+        prediction = mlp(images)
+        loss = loss_func(prediction, labels)
+
+        #backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    print(f"Epoca numero {epoch}, con loss = {loss.item()}")
+    
+
+
+
+
