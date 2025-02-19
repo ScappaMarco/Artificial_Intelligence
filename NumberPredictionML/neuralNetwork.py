b61dad4bd10fbcd6d1eae75087b2767fff95af4f
ScappaMarco <mscappa0@gmail.com>
2025-02-19 10:12:27 +0100

    del

--- NumberPredictionML/neuralNetwork.py
+++ NumberPredictionML/neuralNetwork.py
@@ -5,21 +5,6 @@ class ImageClassifier(nn.Module):
     #definzione rete neurale tramile MLP (strati lineari)
     def __init__(self):
         super().__init__()
-        '''
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1),  # Conv1: (28x28) → (28x28)
-            nn.ReLU(),  
-            
-            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1),  # Conv2: (28x28) → (28x28)
-            nn.ReLU(),  
-            
-            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1),  # Conv3: (28x28) → (28x28)
-            nn.ReLU(),  
-            
-            nn.Flatten(),  # Appiattisce i dati (28x28x128 → 100352)
-            nn.Linear(28 * 28 * 128, 10)  # Layer finale per classificazione (100352 → 10 classi)
-        )
-        '''
         self.model = nn.Sequential(
             nn.Flatten(), #tramiote la funzione Flatten() appiattisco l'immagine 28x28 in un vettore 
             nn.Linear(28*28, 128), #primo strato connesso (28x28 neuroni di input, 128 neuroni di output)
